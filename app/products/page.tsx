9074f0aa9c6206c614e22eacf50b37371fa629bb
SaiAmirthesh <saiamirthesh8419@gmail.com>
2026-02-26 19:28:48 +0530

    feat: Landing Page components

---
+++ app/products/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+const Products = () => {
+  return (
+    <>
+        <div>Products</div>
+        <Button variant="destructive">clickMe</Button>
+    </>
+  )
+}
+
+export default Products
